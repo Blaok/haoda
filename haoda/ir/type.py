95b60d13a86f704ade70b735c1f3fd4bdef40bb5
Ruola <>
2020-01-18 12:33:09 -0800

    import cached_property

--- haoda/ir/type.py
+++ haoda/ir/type.py
@@ -1,20 +1,8 @@
 from typing import Any
 
+from cached_property import cached_property
+
 import haoda.util
-
-class cached_property(object):
-  """
-  Descriptor (non-data) for building an attribute on-demand on first use.
-  """
-  def __init__(self, factory):
-    self._attr_name = factory.__name__
-    self._factory = factory
-
-  def __get__(self, instance, owner):
-    attr = self._factory(instance)
-    setattr(instance, self._attr_name, attr)
-    return attr
-
 
 TYPE_WIDTH = {'float': 32, 'double': 64, 'half': 16}
 
