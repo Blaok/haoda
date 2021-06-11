514ca5c8d3341df61d36b885f5b67403ac7c337e
Blaok <i@blaok.me>
2021-06-10 17:29:43 -0700

    feat: report dir utilization

--- haoda/report/xilinx/rtl/generator.py
+++ haoda/report/xilinx/rtl/generator.py
@@ -22,6 +22,9 @@ if {{ $ips ne "" }} {{
   read_ip $ips
   generate_target synthesis [ get_files *.xci ]
 }}
+foreach tcl_file [glob -nocomplain {hdl_dir}/*.tcl] {{
+  source $tcl_file
+}}
 {set_parallel}
 synth_design {synth_args}
 opt_design
@@ -29,19 +32,104 @@ report_utilization {report_util_args}
 '''
 
 
-class ReportXoUtil(backend.Vivado):
+class ReportDirUtil(backend.Vivado):
   """Run synthesis and generate resource utilization report.
 
   This class is a child of subprocess.Popen and will launch Vivado to run
-  syntheis and generate resource utilization report. Arguments passed to
+  synthesis and generate resource utilization report. Arguments passed to
+  synth_design and report_utilization can be configured via the kwargs params.
+
+  Attributes:
+    job_server_fd: Optional file descriptor of a job server.
+    num_jobs: Optional number of jobs.
+  """
+
+  def __init__(
+      self,
+      hdl_dir: str,
+      rpt_path: str,
+      top_name: str,
+      part_num: Optional[str] = None,
+      synth_kwargs: Optional[Dict[str, str]] = None,
+      report_util_kwargs: Optional[Dict[str, str]] = None,
+  ):
+    """Run synthesis and generate resource utilization report.
+
+    Args:
+      hdl_dir: HDL directory containing *.v and *.tcl files.
+      rpt_file: Path of generated resource utilization report.
+      top_name: Top module name.
+      part_num: Part number; infered from HDL if empty.
+      synth_kwargs: Additional arguments for synth_design.
+      report_util_kwargs: Additional arguments for report_utilization.
+
+    Raises:
+      InputError if part number cannot be inferred.
+    """
+    if part_num is None:
+      for hdl_file_format in ('{}.v', '{0}_{0}.v'):
+        try:
+          with open(os.path.join(
+              hdl_dir,
+              hdl_file_format.format(top_name),
+          )) as hdl_file:
+            part_num = RtlHlsInfo(hdl_file)['HLS_INPUT_PART']
+          break
+        except FileNotFoundError:
+          pass
+    if part_num is None:
+      raise util.InputError('failed to infer part number')
+
+    if synth_kwargs is None:
+      synth_kwargs = {}
+    synth_kwargs['top'] = top_name
+    synth_kwargs['part'] = part_num
+
+    if report_util_kwargs is None:
+      report_util_kwargs = {}
+    report_util_kwargs['file'] = os.path.abspath(rpt_path)
+    report_util_kwargs.setdefault('hierarchical', '')
+
+    synth_args = ' '.join(f'-{k} {v}' for k, v in synth_kwargs.items())
+    report_util_args = ' '.join(
+        f'-{k} {v}' for k, v in report_util_kwargs.items())
+    kwargs = {
+        'hdl_dir': os.path.abspath(hdl_dir),
+        'synth_args': synth_args,
+        'report_util_args': report_util_args,
+        'set_parallel': '',
+    }
+    self.job_server_fd = util.get_job_server_fd(())
+    if self.job_server_fd is not None:
+      new_fd = os.open('/proc/self/fd/%d' % self.job_server_fd,
+                       os.O_RDWR | os.O_NONBLOCK)
+      self.num_jobs = 1  # maximum supported by Vivado is 8
+      try:
+        for i in range(7):
+          self.num_jobs += len(os.read(new_fd, 1))
+      except BlockingIOError as e:
+        pass
+      os.close(new_fd)
+      kwargs['set_parallel'] = 'set_param general.maxThreads %d' % self.num_jobs
+    super().__init__(REPORT_UTIL_COMMANDS.format(**kwargs))
+
+  def __exit__(self, *args):
+    super().__exit__(*args)
+    if self.job_server_fd is not None:
+      os.write(self.job_server_fd, b'x' * (self.num_jobs - 1))
+
+
+class ReportXoUtil(ReportDirUtil):
+  """Run synthesis and generate resource utilization report.
+
+  This class is a child of subprocess.Popen and will launch Vivado to run
+  synthesis and generate resource utilization report. Arguments passed to
   synth_design and report_utilization can be configured via the kwargs params.
 
   Attributes:
     tmpdir: Temporary working directory for the RTL files and generated report.
     rpt_file: File object of generated resource utilization report.
     rpt_file_name: Name of the generated temporary resource utilization report.
-    job_server_fd: Optional file descriptor of a job server.
-    num_jobs: Optional number of jobs.
   """
 
   def __init__(self,
@@ -56,14 +144,16 @@ class ReportXoUtil(backend.Vivado):
     Args:
       xo_file: XO file object containing the HDL files.
       rpt_file: File object of generated resource utilization report.
-      top_name: Optionally specify a different top name other than "Dataflow".
+      top_name: Optionally specify a different top name other than "Dataflow";
+          inferred from XO file if empty.
       part_num: Optionally specify a different part number other than
           automatically determined.
       synth_kwargs: Dict of arguments for the synth_design command.
       report_util_kwargs: Dict of arguments for the synth_design command.
 
     Raises:
-      InputError if input is not a valid XO.
+      InputError: if input is not a valid XO.
+      InternalError: if the report is not generated as expected.
     """
     self.tmpdir = tempfile.TemporaryDirectory(prefix='report-xo-util-')
     self.rpt_file = rpt_file
@@ -84,59 +174,26 @@ class ReportXoUtil(backend.Vivado):
                             name for name in xo_zip.namelist()
                             if name.startswith(hdl_dir_prefix)
                         ])
-      if part_num is None:
-        for hdl_file_format in ('{}.v', '{0}_{0}.v'):
-          try:
-            with open(os.path.join(
-                hdl_dir, hdl_file_format.format(kernel_name))) as hdl_file:
-              part_num = RtlHlsInfo(hdl_file)['HLS_INPUT_PART']
-            break
-          except FileNotFoundError:
-            pass
-    assert part_num is not None
-    if synth_kwargs is None:
-      synth_kwargs = {}
-    if report_util_kwargs is None:
-      report_util_kwargs = {}
-    synth_kwargs.setdefault('top', top_name)
-    synth_kwargs.setdefault('part', part_num)
-    report_util_kwargs.setdefault('hierarchical', '')
-    report_util_kwargs.setdefault('hierarchical_depth', '1')
-    report_util_kwargs.setdefault('file', self.rpt_file_name)
 
-    synth_args = ' '.join('-{} {}'.format(*kv) for kv in synth_kwargs.items())
-    report_util_args = ' '.join(
-        '-{} {}'.format(*kv) for kv in report_util_kwargs.items())
-    kwargs = {
-        'output_dir': os.path.join(self.tmpdir.name, 'output'),
-        'hdl_dir': hdl_dir,
-        'synth_args': synth_args,
-        'report_util_args': report_util_args,
-        'set_parallel': '',
-    }
-    self.job_server_fd = util.get_job_server_fd(())
-    if self.job_server_fd is not None:
-      new_fd = os.open('/proc/self/fd/%d' % self.job_server_fd,
-                       os.O_RDWR | os.O_NONBLOCK)
-      self.num_jobs = 1
-      try:
-        for i in range(7):
-          self.num_jobs += len(os.read(new_fd, 1))
-      except BlockingIOError as e:
-        pass
-      os.close(new_fd)
-      kwargs['set_parallel'] = 'set_param general.maxThreads %d' % self.num_jobs
-    super().__init__(REPORT_UTIL_COMMANDS.format(**kwargs))
+    if not top_name:
+      top_name = kernel_name
+
+    super().__init__(
+        hdl_dir,
+        self.rpt_file_name,
+        top_name,
+        part_num,
+        synth_kwargs,
+        report_util_kwargs,
+    )
 
   def __exit__(self, *args):
     super().__exit__(*args)
-    if self.job_server_fd is not None:
-      os.write(self.job_server_fd, b'x' * (self.num_jobs - 1))
     try:
       with open(self.rpt_file_name) as src_rpt_file:
         shutil.copyfileobj(src_rpt_file, self.rpt_file)
     except FileNotFoundError:
-      raise util.InternalError('failed to generated report file')
+      raise util.InternalError('failed to generate report file')
     self.tmpdir.cleanup()
 
 
