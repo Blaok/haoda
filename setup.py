fc3359fb13c42b6281f60b4967f9ea508122690b
Blaok <i@blaok.me>
2021-09-30 16:48:37 -0700

    chore: bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='haoda',
-    version='0.0.20210801.dev1',
+    version='0.0.20211001.dev1',
     description='Hardware-aware optimization and design automation',
     long_description=long_description,
     long_description_content_type='text/markdown',
